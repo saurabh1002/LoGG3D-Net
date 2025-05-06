50d692f02f212f670a58202ea7be33ea12c35d6b
ssg1002 <sgupta2@uni-bonn.de>
2025-05-06 15:19:58 +0200

    fix bug

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -3,6 +3,7 @@ import os
 import sys
 import torch
 import pickle
+import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 from evaluation.eval_sequence import evaluate_sequence_reg
